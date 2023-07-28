b966b213fc6903ab0bfb9320036b4576fed0dce1
William Reima <william.reima@gmail.com>
2023-07-29 00:00:06 +0300

    started cps component

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+
+use tauri::SystemTray;
+
+
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,6 +13,9 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+
+
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
--- src/components/Cps.jsx
+++ src/components/Cps.jsx
@@ -1,5 +1,25 @@
+import { useState } from "react";
+import "./Cps.css";
 const Cps = () => {
-  return <button>test</button>;
+  const [start, setStart] = useState("Start");
+
+  return (
+    <div>
+      <label>Click per second</label>
+      <input type="number" min="0" max="5"></input>
+      <button
+        onClick={() => {
+          if (start == "Start") {
+            setStart("Stop");
+          } else {
+            setStart("Start");
+          }
+        }}
+      >
+        {start}
+      </button>
+    </div>
+  );
 };
 
 export default Cps;
